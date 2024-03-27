df6ad8d966e7f2a08b91746f683e68c197853618
chaihahaha <chai836275709@gmail.com>
2024-03-27 08:50:01 +0800

    move normalize to linalg

--- linalg/__init__.py
+++ linalg/__init__.py
@@ -7,3 +7,7 @@ def matvec(x, y, transpose_a=False):
         return np.matmul(np.swapaxes(x, 1, 2), y[..., None]).squeeze(-1)
     else:
         return np.matmul(x, y[..., None]).squeeze(-1)
+
+def normalize(x, axis=0):
+    n = np.math.norm(x, axis=axis, keepdims=True)
+    return np.nan_to_num(x/n), n
